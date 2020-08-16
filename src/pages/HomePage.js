532e7ff3e5dcf7c94a31bd614bc40bf57099efd2
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-16 04:20:08 -0700

    created a folder for my blog pages components and a file for the home page.   I  also created a  couple of sentences inside of a function I created

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => (
+  <h1>Hello, welcome to my blog!</h1>
+  <p>
+    Welcome to my blog! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
+  </p>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
+  </p>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
+  </p>
+)
