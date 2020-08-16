3d47da500675b0cb090100b683399e1a4c029064
Leonardo Escudero <leonardolearns@gmail.com>
2020-08-16 04:22:35 -0700

    created a fragment and did a shortcut by importing fragment

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import { Fragment } from 'react';
 const HomePage = () => (
-  <h1>Hello, welcome to my blog!</h1>
-  <p>
-    Welcome to my blog! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
+  <Fragment>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      Welcome to my blog! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
   </p>
-  <p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
   </p>
-  <p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus odit doloribus cum. Voluptatem dignissimos illum quo earum dicta, inventore tenetur adipisci delectus. Quis ex cumque, in similique animi at.
   </p>
+  </Fragment>
 )
